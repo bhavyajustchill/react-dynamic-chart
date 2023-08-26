d8cf1b89c1a13bf837cec954ecdb0b33576df709
Bhavya Popat <bhavyabpopat7777@gmail.com>
2023-08-26 12:06:24 +0530

    Minor UI adjustments and Code Cleanup

--- src/App.js
+++ src/App.js
@@ -1,267 +1,159 @@
-import React, { useState } from 'react';
-import './App.css';
-import { PrimeReactProvider } from 'primereact/api';
-import DynamicPieChart from './components/DynamicPieChart';
+import React, { useState } from "react";
+import "./App.css";
+import { PrimeReactProvider } from "primereact/api";
+import DynamicPieChart from "./components/DynamicPieChart";
 
 function App() {
+  const [sampleChartData, setSampleChartData] = useState([
+    {
+      program: "B.C.A.",
+      year: "2018-19",
+      columns: ["Higher Studies", "Job", "Entrepreneur/Businessman", "Other"],
+      row: [11, 10, 26, 50],
+      backgroundColor: ["#00cc99", "#C55858", "#3883F5", "#ffff00"],
+      hoverBackgroundColor: ["#00cc99", "#C55858", "#3883F5", "#ffff00"],
+      pdfUrl:
+        "https://www.meity.gov.in/writereaddata/files/National-Data-Governance-Framework-Policy.pdf",
+    },
+    {
+      program: "B.Sc.I.T.",
+      year: "2018-19",
+      columns: ["Higher Studies", "Job", "Entrepreneur/Businessman", "Other"],
+      row: [18, 9, 8, 61],
+      backgroundColor: ["#00cc99", "#C55858", "#3883F5", "#ffff00"],
+      hoverBackgroundColor: ["#00cc99", "#C55858", "#3883F5", "#ffff00"],
+      pdfUrl:
+        "https://www.meity.gov.in/writereaddata/files/National-Data-Governance-Framework-Policy.pdf",
+    },
+    {
+      program: "M.Sc.I.T.",
+      year: "2018-19",
+      columns: ["Higher Studies", "Job", "Entrepreneur/Businessman", "Other"],
+      row: [0, 30, 0, 0],
+      backgroundColor: ["#00cc99", "#C55858", "#3883F5", "#ffff00"],
+      hoverBackgroundColor: ["#00cc99", "#C55858", "#3883F5", "#ffff00"],
+      pdfUrl:
+        "https://www.meity.gov.in/writereaddata/files/National-Data-Governance-Framework-Policy.pdf",
+    },
+    {
+      program: "B.C.A.",
+      year: "2019-20",
+      columns: ["Higher Studies", "Job", "Entrepreneur/Businessman", "Other"],
+      row: [30, 22, 13, 26],
+      backgroundColor: ["#00cc99", "#C55858", "#3883F5", "#ffff00"],
+      hoverBackgroundColor: ["#00cc99", "#C55858", "#3883F5", "#ffff00"],
+      pdfUrl:
+        "https://www.meity.gov.in/writereaddata/files/National-Data-Governance-Framework-Policy.pdf",
+    },
+    {
+      program: "B.Sc.I.T.",
+      year: "2019-20",
+      columns: ["Higher Studies", "Job", "Entrepreneur/Businessman", "Other"],
+      row: [27, 21, 18, 49],
+      backgroundColor: ["#00cc99", "#C55858", "#3883F5", "#ffff00"],
+      hoverBackgroundColor: ["#00cc99", "#C55858", "#3883F5", "#ffff00"],
+      pdfUrl:
+        "https://www.meity.gov.in/writereaddata/files/National-Data-Governance-Framework-Policy.pdf",
+    },
+    {
+      program: "M.Sc.I.T.",
+      year: "2019-20",
+      columns: ["Higher Studies", "Job", "Entrepreneur/Businessman", "Other"],
+      row: [0, 22, 4, 3],
+      backgroundColor: ["#00cc99", "#C55858", "#3883F5", "#ffff00"],
+      hoverBackgroundColor: ["#00cc99", "#C55858", "#3883F5", "#ffff00"],
+      pdfUrl:
+        "https://www.meity.gov.in/writereaddata/files/National-Data-Governance-Framework-Policy.pdf",
+    },
+    {
+      program: "B.C.A.",
+      year: "2020-21",
+      columns: ["Higher Studies", "Job", "Entrepreneur/Businessman", "Other"],
+      row: [34, 33, 12, 21],
+      backgroundColor: ["#00cc99", "#C55858", "#3883F5", "#ffff00"],
+      hoverBackgroundColor: ["#00cc99", "#C55858", "#3883F5", "#ffff00"],
+      pdfUrl:
+        "https://www.meity.gov.in/writereaddata/files/National-Data-Governance-Framework-Policy.pdf",
+    },
+    {
+      program: "B.Sc.I.T.",
+      year: "2020-21",
+      columns: ["Higher Studies", "Job", "Entrepreneur/Businessman", "Other"],
+      row: [35, 28, 18, 19],
+      backgroundColor: ["#00cc99", "#C55858", "#3883F5", "#ffff00"],
+      hoverBackgroundColor: ["#00cc99", "#C55858", "#3883F5", "#ffff00"],
+      pdfUrl:
+        "https://www.meity.gov.in/writereaddata/files/National-Data-Governance-Framework-Policy.pdf",
+    },
+    {
+      program: "M.Sc.I.T.",
+      year: "2020-21",
+      columns: ["Higher Studies", "Job", "Entrepreneur/Businessman", "Other"],
+      row: [2, 10, 2, 1],
+      backgroundColor: ["#00cc99", "#C55858", "#3883F5", "#ffff00"],
+      hoverBackgroundColor: ["#00cc99", "#C55858", "#3883F5", "#ffff00"],
+      pdfUrl:
+        "https://www.meity.gov.in/writereaddata/files/National-Data-Governance-Framework-Policy.pdf",
+    },
+    {
+      program: "B.C.A.",
+      year: "2021-22",
+      columns: ["Higher Studies", "Job", "Entrepreneur/Businessman", "Other"],
+      row: [61, 43, 22, 20],
+      backgroundColor: ["#00cc99", "#C55858", "#3883F5", "#ffff00"],
+      hoverBackgroundColor: ["#00cc99", "#C55858", "#3883F5", "#ffff00"],
+      pdfUrl:
+        "https://www.meity.gov.in/writereaddata/files/National-Data-Governance-Framework-Policy.pdf",
+    },
+    {
+      program: "B.Sc.I.T.",
+      year: "2021-22",
+      columns: ["Higher Studies", "Job", "Entrepreneur/Businessman", "Other"],
+      row: [41, 22, 14, 20],
+      backgroundColor: ["#00cc99", "#C55858", "#3883F5", "#ffff00"],
+      hoverBackgroundColor: ["#00cc99", "#C55858", "#3883F5", "#ffff00"],
+      pdfUrl:
+        "https://www.meity.gov.in/writereaddata/files/National-Data-Governance-Framework-Policy.pdf",
+    },
+    {
+      program: "M.Sc.I.T.",
+      year: "2021-22",
+      columns: ["Higher Studies", "Job", "Entrepreneur/Businessman", "Other"],
+      row: [0, 22, 7, 5],
+      backgroundColor: ["#00cc99", "#C55858", "#3883F5", "#ffff00"],
+      hoverBackgroundColor: ["#00cc99", "#C55858", "#3883F5", "#ffff00"],
+      pdfUrl:
+        "https://www.meity.gov.in/writereaddata/files/National-Data-Governance-Framework-Policy.pdf",
+    },
+  ]);
 
-  const sampleChartData = [
-    {
-      program:"B.C.A.",
-      year:"2018-19",
-      columns:['Higher Studies', 'Job', 'Entrepreneur/Businessman', 'Other'], 
-      row:[11,10,26,50], 
-      backgroundColor:[
-        "#00cc99", 
-        "#C55858", 
-        "#3883F5",
-        "#ffff00"
-    ], 
-      hoverBackgroundColor:[
-        "#00cc99", 
-        "#C55858", 
-        "#3883F5",
-        "#ffff00"
-    ],
-      pdfUrl:"https://www.meity.gov.in/writereaddata/files/National-Data-Governance-Framework-Policy.pdf"
-    },
-    {
-      program:"B.Sc.I.T.",
-      year:"2018-19",
-      columns:['Higher Studies', 'Job', 'Entrepreneur/Businessman', 'Other'], 
-      row:[18,9,8,61], 
-      backgroundColor:[
-        "#00cc99", 
-        "#C55858", 
-        "#3883F5",
-        "#ffff00"
-    ], 
-      hoverBackgroundColor:[
-        "#00cc99", 
-        "#C55858", 
-        "#3883F5",
-        "#ffff00"
-    ],
-      pdfUrl:"https://www.meity.gov.in/writereaddata/files/National-Data-Governance-Framework-Policy.pdf"
-    },
-    {
-      program:"M.Sc.I.T.",
-      year:"2018-19",
-      columns:['Higher Studies', 'Job', 'Entrepreneur/Businessman', 'Other'], 
-      row:[0,30,0,0], 
-      backgroundColor:[
-        "#00cc99", 
-        "#C55858", 
-        "#3883F5",
-        "#ffff00"
-    ], 
-      hoverBackgroundColor:[
-        "#00cc99", 
-        "#C55858", 
-        "#3883F5",
-        "#ffff00"
-    ],
-      pdfUrl:"https://www.meity.gov.in/writereaddata/files/National-Data-Governance-Framework-Policy.pdf"
-    },{
-      program:"B.C.A.",
-      year:"2019-20",
-      columns:['Higher Studies', 'Job', 'Entrepreneur/Businessman', 'Other'], 
-      row:[30,22,13,26], 
-      backgroundColor:[
-        "#00cc99", 
-        "#C55858", 
-        "#3883F5",
-        "#ffff00"
-    ], 
-      hoverBackgroundColor:[
-        "#00cc99", 
-        "#C55858", 
-        "#3883F5",
-        "#ffff00"
-    ],
-      pdfUrl:"https://www.meity.gov.in/writereaddata/files/National-Data-Governance-Framework-Policy.pdf"
-    },
-    {
-      program:"B.Sc.I.T.",
-      year:"2019-20",
-      columns:['Higher Studies', 'Job', 'Entrepreneur/Businessman', 'Other'], 
-      row:[27,21,18,49], 
-      backgroundColor:[
-        "#00cc99", 
-        "#C55858", 
-        "#3883F5",
-        "#ffff00"
-    ], 
-      hoverBackgroundColor:[
-        "#00cc99", 
-        "#C55858", 
-        "#3883F5",
-        "#ffff00"
-    ],
-      pdfUrl:"https://www.meity.gov.in/writereaddata/files/National-Data-Governance-Framework-Policy.pdf"
-    },
-    {
-      program:"M.Sc.I.T.",
-      year:"2019-20",
-      columns:['Higher Studies', 'Job', 'Entrepreneur/Businessman', 'Other'], 
-      row:[0,22,4,3], 
-      backgroundColor:[
-        "#00cc99", 
-        "#C55858", 
-        "#3883F5",
-        "#ffff00"
-    ], 
-      hoverBackgroundColor:[
-        "#00cc99", 
-        "#C55858", 
-        "#3883F5",
-        "#ffff00"
-    ],
-      pdfUrl:"https://www.meity.gov.in/writereaddata/files/National-Data-Governance-Framework-Policy.pdf"
-    },{
-      program:"B.C.A.",
-      year:"2020-21",
-      columns:['Higher Studies', 'Job', 'Entrepreneur/Businessman', 'Other'], 
-      row:[34,33,12,21], 
-      backgroundColor:[
-        "#00cc99", 
-        "#C55858", 
-        "#3883F5",
-        "#ffff00"
-    ], 
-      hoverBackgroundColor:[
-        "#00cc99", 
-        "#C55858", 
-        "#3883F5",
-        "#ffff00"
-    ],
-      pdfUrl:"https://www.meity.gov.in/writereaddata/files/National-Data-Governance-Framework-Policy.pdf"
-    },
-    {
-      program:"B.Sc.I.T.",
-      year:"2020-21",
-      columns:['Higher Studies', 'Job', 'Entrepreneur/Businessman', 'Other'], 
-      row:[35,28,18,19], 
-      backgroundColor:[
-        "#00cc99", 
-        "#C55858", 
-        "#3883F5",
-        "#ffff00"
-    ], 
-      hoverBackgroundColor:[
-        "#00cc99", 
-        "#C55858", 
-        "#3883F5",
-        "#ffff00"
-    ],
-      pdfUrl:"https://www.meity.gov.in/writereaddata/files/National-Data-Governance-Framework-Policy.pdf"
-    },
-    {
-      program:"M.Sc.I.T.",
-      year:"2020-21",
-      columns:['Higher Studies', 'Job', 'Entrepreneur/Businessman', 'Other'], 
-      row:[2,10,2,1], 
-      backgroundColor:[
-        "#00cc99", 
-        "#C55858", 
-        "#3883F5",
-        "#ffff00"
-    ], 
-      hoverBackgroundColor:[
-        "#00cc99", 
-        "#C55858", 
-        "#3883F5",
-        "#ffff00"
-    ],
-      pdfUrl:"https://www.meity.gov.in/writereaddata/files/National-Data-Governance-Framework-Policy.pdf"
-    },{
-      program:"B.C.A.",
-      year:"2021-22",
-      columns:['Higher Studies', 'Job', 'Entrepreneur/Businessman', 'Other'], 
-      row:[61,43,22,20], 
-      backgroundColor:[
-        "#00cc99", 
-        "#C55858", 
-        "#3883F5",
-        "#ffff00"
-    ], 
-      hoverBackgroundColor:[
-        "#00cc99", 
-        "#C55858", 
-        "#3883F5",
-        "#ffff00"
-    ],
-      pdfUrl:"https://www.meity.gov.in/writereaddata/files/National-Data-Governance-Framework-Policy.pdf"
-    },
-    {
-      program:"B.Sc.I.T.",
-      year:"2021-22",
-      columns:['Higher Studies', 'Job', 'Entrepreneur/Businessman', 'Other'], 
-      row:[41,22,14,20], 
-      backgroundColor:[
-        "#00cc99", 
-        "#C55858", 
-        "#3883F5",
-        "#ffff00"
-    ], 
-      hoverBackgroundColor:[
-        "#00cc99", 
-        "#C55858", 
-        "#3883F5",
-        "#ffff00"
-    ],
-      pdfUrl:"https://www.meity.gov.in/writereaddata/files/National-Data-Governance-Framework-Policy.pdf"
-    },
-    {
-      program:"M.Sc.I.T.",
-      year:"2021-22",
-      columns:['Higher Studies', 'Job', 'Entrepreneur/Businessman', 'Other'], 
-      row:[0,22,7,5], 
-      backgroundColor:[
-        "#00cc99", 
-        "#C55858", 
-        "#3883F5",
-        "#ffff00"
-    ], 
-      hoverBackgroundColor:[
-        "#00cc99", 
-        "#C55858", 
-        "#3883F5",
-        "#ffff00"
-    ],
-      pdfUrl:"https://www.meity.gov.in/writereaddata/files/National-Data-Governance-Framework-Policy.pdf"
-    }
-  ];
-
-  const [fullChartData, setfullChartData] = useState([]);
-
-
-  const columns = ['Higher Studies', 'Job', 'Entrepreneur/Businessman', 'Other'];
-  const row = [61,43,22,20];
-  const backgroundColor = [
-    "#00cc99", 
-    "#C55858", 
-    "#3883F5",
-    "#ffff00"
-];
-  const hoverBackgroundColor = [
-    "#00cc99", 
-    "#C55858",
-    "#3883F5",
-    "#ffff00"
-];
-  
   return (
     <>
-    <div className='container-fluid row d-flex align-items-center justify-content-center me-0'>
-      <h1 className='display-1 text-center my-5'>Placement Data with Dynamic Pie Chart</h1>
-    <PrimeReactProvider>
-      {
-        sampleChartData.map((data, index) => <DynamicPieChart columns={data.columns} row={data.row} backgroundColor={data.backgroundColor} hoverBackgroundColor={data.hoverBackgroundColor} pointStyle={true} year={data.year} pdfUrl={data.pdfUrl} program={data.program} />)
-      }   
-    </PrimeReactProvider>
-
-    </div>
+      <div className="container-fluid">
+        <div className="row d-flex align-items-center justify-content-center me-0">
+          <h3 className="display-3 text-center my-5">Placement Data with Dynamic Pie Chart</h3>
+          <PrimeReactProvider>
+            {sampleChartData.map((data, index) => {
+              return (
+                <>
+                  <div className="my-2 col-md-4 mx-0">
+                    <DynamicPieChart
+                      columns={data.columns}
+                      row={data.row}
+                      backgroundColor={data.backgroundColor}
+                      hoverBackgroundColor={data.hoverBackgroundColor}
+                      pointStyle={true}
+                      year={data.year}
+                      pdfUrl={data.pdfUrl}
+                      program={data.program}
+                    />
+                  </div>
+                </>
+              );
+            })}
+          </PrimeReactProvider>
+        </div>
+      </div>
     </>
   );
 }
--- src/components/DynamicPieChart.js
+++ src/components/DynamicPieChart.js
@@ -1,53 +1,68 @@
-import React, { useState, useEffect } from 'react';
-import { Chart } from 'primereact/chart';
+import React, { useState, useEffect } from "react";
+import { Chart } from "primereact/chart";
 
 export default function DynamicPieChart(props) {
-    const {program, year, columns, row, backgroundColor, hoverBackgroundColor,pdfUrl} = props;
-    const [chartData, setChartData] = useState({});
-    const [chartOptions, setChartOptions] = useState({});
+  const { program, year, columns, row, backgroundColor, hoverBackgroundColor, pdfUrl } = props;
+  const [chartData, setChartData] = useState({});
+  const [chartOptions, setChartOptions] = useState({});
 
-  
+  useEffect(() => {
+    const data = {
+      labels: columns,
+      datasets: [
+        {
+          data: row,
+          backgroundColor: backgroundColor,
+          hoverBackgroundColor: hoverBackgroundColor,
+        },
+      ],
+    };
+    const options = {
+      plugins: {
+        legend: {
+          labels: {
+            usePointStyle: props.pointStyle,
+          },
+        },
+      },
+    };
 
-    useEffect(() => {
-        const data = {
-            labels: columns,
-            datasets: [
-                {
-                    data: row,
-                    backgroundColor: backgroundColor,
-                    hoverBackgroundColor: hoverBackgroundColor
-                }
-            ]
-        };
-        const options = {
-            plugins: {
-                legend: {
-                    labels: {
-                        usePointStyle: props.pointStyle
-                    }
-                }
-            }
-        };
+    setChartData(data);
+    setChartOptions(options);
+  }, [
+    program,
+    year,
+    columns,
+    row,
+    backgroundColor,
+    hoverBackgroundColor,
+    pdfUrl,
+    props.pointStyle,
+  ]);
 
-        setChartData(data);
-        setChartOptions(options);
-    }, []);
-
-    return (
-        <div className='my-2 col-md-6 mx-0 d-flex justify-content-center'>
-        <div className='card' style={{width:'fit-content'}}>
-            <div className='card-header'>
-            <h3 className='text-decoration-underline text-bold text-center'>Consolidate Placement Details {year}</h3>
-            </div>
-            <div className='card-body d-flex align-items-center justify-content-center'>
-                <a href={pdfUrl} target="_blank">
-                    <Chart height='250px' width='250px' type="pie" data={chartData} options={chartOptions} />
-                </a>
-            </div>
-            <div className='card-footer'>
-                <h4 className='text-bold text-center'>Program : {program}</h4>
-            </div>
+  return (
+    <div className="d-flex justify-content-center">
+      <div className="card" style={{ width: "fit-content" }}>
+        <div className="card-header">
+          <h4 className="text-decoration-underline text-bold text-center">
+            Consolidated Placement Details {year}
+          </h4>
         </div>
+        <div className="card-body d-flex align-items-center justify-content-center">
+          <a href={pdfUrl} target="_blank">
+            <Chart
+              height="250px"
+              width="250px"
+              type="pie"
+              data={chartData}
+              options={chartOptions}
+            />
+          </a>
         </div>
-    )
+        <div className="card-footer">
+          <h4 className="text-bold text-center">Program : {program}</h4>
+        </div>
+      </div>
+    </div>
+  );
 }
